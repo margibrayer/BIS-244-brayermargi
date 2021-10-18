7e6800f6f2df6b50fcb0f5c71131074296402489
margibrayer <70169088+margibrayer@users.noreply.github.com>
2021-10-18 16:35:29 -0400

    Add files via upload

---
+++ Assignment 3.R
@@ -0,0 +1,8 @@
+if (!require("here")) install.packages("here")
+library("here")
+library("tidyverse")
+votes <- read_csv(here("Data","PRESIDENT_precinct_general.zip"))
+View(President)
+library(ggplot2)
+
+*.csv
