88059742953006403434a84446fd3f374d1af9f4
margibrayer <70169088+margibrayer@users.noreply.github.com>
2021-11-30 16:18:33 -0500

    Delete Assignment 3.R

--- Assignment 3.R
+++
@@ -1,8 +0,0 @@
-if (!require("here")) install.packages("here")
-library("here")
-library("tidyverse")
-votes <- read_csv(here("Data","PRESIDENT_precinct_general.zip"))
-View(President)
-library(ggplot2)
-
-*.csv
